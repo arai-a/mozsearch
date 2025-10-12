2e661d08c8a13a1c64c2ece95703afe552d4cbfa
Tooru Fujisawa <arai_a@mac.com>
2025-10-12 12:34:41 +0900

    4fix

--- static/js/blame.js
+++ static/js/blame.js
@@ -84,12 +84,14 @@ var BlamePopup = new (class BlamePopup {
 
     let isGC = false;
     let gcInfo = null;
-    for (const sym of elt.dataset.symbols.split(",")) {
-      if (sym in SYM_INFO) {
-        const info = SYM_INFO[sym];
-        if (info.meta && "canGC" in info.meta) {
-          gcInfo = info;
-          isGC = true;
+    if (elt?.dataset?.symbols) {
+      for (const sym of elt.dataset.symbols.split(",")) {
+        if (sym in SYM_INFO) {
+          const info = SYM_INFO[sym];
+          if (info.meta && "canGC" in info.meta) {
+            gcInfo = info;
+            isGC = true;
+          }
         }
       }
     }
